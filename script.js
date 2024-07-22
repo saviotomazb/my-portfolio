315b8b30d170890ad05f0c4e7a2e2f1231201dbf
Savio <saviotomaz_@hotmail.com>
2024-07-22 07:37:04 -0300

    :broom: Removendo comentários

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 document.addEventListener("DOMContentLoaded", function() {
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".nav-menu");
+    const navLink = document.querySelectorAll(".nav-link");
+    navLink.forEach(n => n.addEventListener("click", closeMenu));
+    hamburger.addEventListener("click", mobileMenu);
+    function mobileMenu() {
+        hamburger.classList.toggle("active");
+        navMenu.classList.toggle("active");
+    }
+    function closeMenu() {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    }
+    window.addEventListener('scroll', function() {
+        var header = document.querySelector('.header');
+        if (window.scrollY > 0) {
+            header.classList.add('nav-shadow');
+        } else {
+            header.classList.remove('nav-shadow');
+        }
+    });
     document.body.style.opacity = "1";
     var path = window.location.pathname;
     path = path.replace(/^\/|\/$/g, '');
-    var links = document.querySelectorAll('.menu__itens a');
+    var links = document.querySelectorAll('.nav-link');
     var defaultColor = getComputedStyle(document.documentElement).getPropertyValue('--cor-letra-titulo');
     links.forEach(function(link) {
         var href = link.getAttribute('href');
